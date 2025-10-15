53b2da6200f86dce265e12470d12dde722f82d4d
BHyeonKim <rlaqhguse@gmail.com>
2025-10-15 14:37:30 +0900

    feat: 입력값에서 구분자 및 숫자배열 파싱
    사용자 입력에서 커스텀 구분자와 숫자배열을 분리하는 함수 구현
    - 시작 splitter('//')와 마지막 splitter('\n')의 위치로 구분자의 정상
      존재 여부 판단
    - 구분자가 존재하지 않을경우 입력으로 들어온
      문자열을 튜플 형식([null, 숫자문자열])으로 리턴
    - 구분자가 정상 위치에 존재할경우 튜플 형식([구분자, 숫자문자열])으로 리턴
    - 이외의 경우 비정상 입력으로 판단하여 Error를 Throw

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 ## 기능 목록
 
 > 문자열을 커스텀 구분자와 숫자 문자열로 파싱하여 튜플형태로 반환한다.
-- [ ] 1. 시작 splitter(`//`)와 마지막 splitter(`\n`)의 시작 위치를 `startIndexOfSplitter`와 `endIndexOfSplitter`에 저장한다.
-- [ ] 2. 시작 splitter(`//`)와 마지막 splitter(`\n`)가 둘다 존재하지 않으면(`SPLITTER_NOT_EXISTS`) 입력으로 들어온 문자열을 튜플 형태로 내보낸다.   
+- [x] 1. 시작 splitter(`//`)와 마지막 splitter(`\n`)의 시작 위치를 `startIndexOfSplitter`와 `endIndexOfSplitter`에 저장한다.
+- [x] 2. 시작 splitter(`//`)와 마지막 splitter(`\n`)가 둘다 존재하지 않으면(`SPLITTER_NOT_EXISTS`) 입력으로 들어온 문자열을 튜플 형태로 내보낸다.   
       (`string => [null, string]`)
-- [ ] 3. 시작 splitter(`//`)와 마지막 splitter(`\n`)가 둘다 존재하고 정상 위치에 있으면 `delimiter`와 `numberString`을 파싱하고 리턴한다.   
+- [x] 3. 시작 splitter(`//`)와 마지막 splitter(`\n`)가 둘다 존재하고 정상 위치에 있으면 `delimiter`와 `numberString`을 파싱하고 리턴한다.   
       (`string => [delimiter, numberString]`)
-- [ ] 4. 이외의 경우 입력이 잘못된 것으로 판단하여 Error를 Throw 한다.
+- [x] 4. 이외의 경우 입력이 잘못된 것으로 판단하여 Error를 Throw 한다.
 
 
 > 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환한다.
@@ -30,7 +30,7 @@
 ## 실행 흐름
 - [x] 1. 사용자의 입력값을 받습니다.   
 (형식: "" => 0, "1,2" => 3, "1,2,3" => 6, "1,2:3" => 6, ?//;\n1;2;3" => 6)
-- [ ] 2. 입력값을 구분자와 나머지 문자열로 파싱합니다.   
+- [x] 2. 입력값을 구분자와 나머지 문자열로 파싱합니다.   
       2-1. 시작 splitter(`//`)와 마지막 splitter(`\n`)가 둘다 존재하지 않으면(`SPLITTER_NOT_EXISTS`) 커스텀 구분자가 없는것으로 판단하고 입력으로 들어온 문자열을 내보냅니다.   
       2-2. 시작 splitter(`//`)와 마지막 splitter(`\n`)가 둘다 존재하고 정상 위치에 있으면(`START_SPLITTER_POSITION` ,`END_SPLITTER_POSITION`) `delimiter`와 `numberString`을 파싱하고 리턴합니다.   
       2-3. 이외의 입력값은 오류로 판단하여 Error를 Throw 한다.
--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import { Console } from '@woowacourse/mission-utils';
 
+const START_SPLITTER = '//';
+const END_SPLITTER = '\\n';
+
+const SPLITTER_LENGTH = START_SPLITTER.length;
+const DELIMITER_CHARACTER_LENGTH = 1;
+
+const START_SPLITTER_POSITION = 0;
+const END_SPLITTER_POSITION = SPLITTER_LENGTH + DELIMITER_CHARACTER_LENGTH;
+
+const SPLITTER_NOT_EXISTS = -1;
+
 class App {
   async run() {
     const userInput = await this.getUserInput();
 
-    Console.print('결과 : ' + userInput);
+    const [delimiter, numString] =this.parseStringToDelimiterAndNumberString(userInput);
+
+    Console.print('구분자 : ' + delimiter);
+    Console.print('숫자배열 : ' + numString);
   }
 
   async getUserInput(){
@@ -14,6 +28,26 @@ class App {
       return userInput;
     } catch (error){
       throw new Error('입력과정에서 오류가 발생했습니다.');
+    }
+  }
+
+  parseStringToDelimiterAndNumberString(string){
+    const startIndexOfSplitter = string.indexOf(START_SPLITTER);
+    const endIndexOfSplitter = string.indexOf(END_SPLITTER);
+    
+
+    if(startIndexOfSplitter === SPLITTER_NOT_EXISTS && endIndexOfSplitter === SPLITTER_NOT_EXISTS){
+      return [null, string];
+    }
+
+
+    if(startIndexOfSplitter === START_SPLITTER_POSITION && endIndexOfSplitter === END_SPLITTER_POSITION ){
+      const delimiter = string.slice(startIndexOfSplitter + SPLITTER_LENGTH, endIndexOfSplitter);
+      const numberString = string.slice(endIndexOfSplitter + SPLITTER_LENGTH);
+
+      return [delimiter, numberString]
+    }else{
+      throw new Error('구분자의 입력이 잘못되었습니다.')
     }
   }
 }
