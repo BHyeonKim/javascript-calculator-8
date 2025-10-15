0ed532489111de3ba8a36f5066500dfc21125a15
BHyeonKim <rlaqhguse@gmail.com>
2025-10-15 13:10:11 +0900

    feat: 사용자 입력 기능 추가
    `@woowacourse/mission-utils` 라이브러리를 사용하여 입력기능 추가
    - getUserInput 메서드에서 사용자 입력값을 받음
    - 입력과정에서 에러가 있을경우 Error를 Throw

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    const userInput = await this.getUserInput();
+
+    Console.print('결과 : ' + userInput);
+  }
+
+  async getUserInput(){
+    try{
+      const userInput = await Console.readLineAsync('덧샘할 문자열을 입력해 주세요.\n');
+      
+      return userInput;
+    } catch (error){
+      throw new Error('입력과정에서 오류가 발생했습니다.');
+    }
+  }
 }
+
+
 
 export default App;
