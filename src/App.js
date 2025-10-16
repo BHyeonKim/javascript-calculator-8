2efce2bc7f929bd4a2423a8b8e6c82e65ca9fba8
BHyeonKim <rlaqhguse@gmail.com>
2025-10-16 14:38:16 +0900

    refactor: 에러 메시지 상수로 분리
    매직리터럴인 에러 메시지를 상수 파일('messages.js')로 분리했습니다.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
   START_SPLITTER,
   START_SPLITTER_POSITION,
 } from './constants';
+import MESSAGE from './messages'
 
 class App {
   async run() {
@@ -30,7 +31,7 @@ class App {
       if (error instanceof Error) {
         errorMessage += error.message;
       } else {
-        errorMessage += '실행중에 에러가 발생했습니다.';
+        errorMessage += MESSAGE.ERROR_RUNTIME;
       }
 
       throw new Error(errorMessage);
@@ -40,12 +41,12 @@ class App {
   async getUserInput() {
     try {
       const userInput = await Console.readLineAsync(
-        '덧샘할 문자열을 입력해 주세요.\n',
+        `${MESSAGE.MESSAGE_INTRODUCE}\n`,
       );
 
       return userInput;
     } catch {
-      throw new Error('입력과정에서 오류가 발생했습니다.');
+      throw new Error(MESSAGE.ERROR_INPUT);
     }
   }
 
@@ -72,7 +73,7 @@ class App {
 
       return [delimiter, numberString];
     }
-    throw new Error('구분자의 입력이 잘못되었습니다.');
+    throw new Error(MESSAGE.ERROR_INPUT_WRONG_DELIMITER);
   }
 
   generateCustomSplitFunction(delimiter = '') {
@@ -91,7 +92,7 @@ class App {
 
   convertArrToNumArr(arr) {
     if (!Array.isArray(arr)) {
-      throw new Error('배열이 아닙니다.');
+      throw new Error(MESSAGE.ERROR_NOT_ARRAY);
     }
 
     const numArr = [];
@@ -100,11 +101,11 @@ class App {
       const num = Number(item);
 
       if (Number.isNaN(num)) {
-        throw new Error('배열에 숫자가 아닌 값이 들어있습니다.');
+        throw new Error(MESSAGE.ERROR_NOT_NUMBER);
       }
 
       if (num <= 0) {
-        throw new Error('숫자는 양수만 가능합니다.');
+        throw new Error(MESSAGE.ERROR_ONLY_POSITIVE);
       }
 
       numArr.push(num);
---
+++ src/messages.js
@@ -0,0 +1,19 @@
+export const MESSAGE_INTRODUCE = '덧샘할 문자열을 입력해 주세요.'
+
+export const ERROR_RUNTIME = '실행중에 에러가 발생했습니다.'
+export const ERROR_INPUT = '입력과정에서 오류가 발생했습니다.'
+export const ERROR_INPUT_WRONG_DELIMITER = '구분자의 입력이 잘못되었습니다.'
+export const ERROR_NOT_ARRAY = '배열이 아닙니다.'
+export const ERROR_NOT_NUMBER = '숫자가 아닙니다.'
+export const ERROR_ONLY_POSITIVE = '숫자는 양수만 가능합니다.'
+
+
+export default {
+  MESSAGE_INTRODUCE,
+  ERROR_RUNTIME,
+  ERROR_INPUT,
+  ERROR_INPUT_WRONG_DELIMITER,
+  ERROR_NOT_ARRAY,
+  ERROR_NOT_NUMBER,
+  ERROR_ONLY_POSITIVE,
+}
