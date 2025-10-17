be352b816d2b28683e7d001bdd04783ced7a6c94
BHyeonKim <rlaqhguse@gmail.com>
2025-10-17 11:33:09 +0900

    chore(package): eslint 및 prettier 설치
    eslint
    - 린팅을 위해 추가
    - prettier와 충돌을 막기 위해 eslint-config-prettier 설정 사용
    - eslint-config-airbnb-base 설정 사용
    - 위의 config와 flat-config-system(eslint.config.js)의 호환이 안되
      eslintrc 설정파일 사용
    prettier
    - 포멧팅을 위해 추가
    package.json 수정
    - scripts에 lint 스크립트 추가

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+// .eslintrc.js
+
+module.exports = {
+    ignorePatterns: ["package*", ".npmrc", "*.md", ".*", "__tests__"],
+    extends: [
+      "eslint:recommended",
+      "airbnb-base",
+      "prettier"
+    ],
+};
