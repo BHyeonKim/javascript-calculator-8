140602393d3a29545ea55d83338abecfddbe650b
BHyeonKim <rlaqhguse@gmail.com>
2025-10-19 22:40:21 +0900

    chore(eslint): parser option 2022로 설정
    이 옵션이 없을 시 eslint가 index.js의 top level await을 에러로 판단.
    - top level await을 허용하기 위해 parser option에ecma version을 2022로
      설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,13 @@
 // .eslintrc.js
 
 module.exports = {
+    env:{
+      node: true,
+    },
+    parserOptions:{
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
     ignorePatterns: ["package*", ".npmrc", "*.md", ".*", "__tests__"],
     extends: [
       "eslint:recommended",
